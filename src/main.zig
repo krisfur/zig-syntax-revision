01aca2d9bd2783a311a87e1367cd4ac56c4c0d93
Krzysztof Furman <krisfur@proton.me>
2025-11-29 10:43:58 +0000

    Started basics.

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,31 @@
-const std = @import("std");
+const std = @import("std"); // import as expected
+const print = std.debug.print; // you can also do aliasing here, a bit pythonic
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
+    // There are no multiline comments in zig, git gud or use your IDEs ctrl+/
+    // using /// makes doc comments that get joined together into one multiline bit of documentation
+
+    // Print to stderr:
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // without macros to print to stdout or stderr:
+    try std.io.getStdOut().writer().writeAll("Hello, World!\n"); //note that the official docs for 0.15.2 are wrong on this
+
+    // VALUES
+
+    const integer: i32 = 1 + 3; //const cannot be changed
+    var floaty: f64 = 32.0; //var can be changed
+    floaty += 37.0;
+    print("We have {} of type {} and {} of type {}.", .{ integer, @TypeOf(integer), floaty, @TypeOf(floaty) });
+    //print(integer + floaty); would fail
+    print("\n{}+{}={}\n", .{ integer, floaty, integer + floaty }); //adding int and float gives float, no type error
+    const myString: []const u8 = "hewwo"; //strings are arrays of u8, so UTF-8 supported
+    print("{s}\n", .{myString}); //fmt needs to know that this is meant as a string literal
 }
 
-test "simple test" {
+test "simple test" { //tests run with "zig test ./src/main.zig"
     const gpa = std.testing.allocator;
-    var list = std.ArrayList(i32).init(gpa); // Initialize with the allocator
-    defer list.deinit(); // deinit no longer takes an allocator argument
-    try list.append(42); // append no longer takes an allocator argument
+    var list = std.ArrayList(i32).init(gpa);
+    defer list.deinit();
+    try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
