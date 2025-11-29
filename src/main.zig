6d2cc500a250f2874e4db71f4beec78464682f1d
Krzysztof Furman <krisfur@proton.me>
2025-11-29 21:34:45 +0000

    Basic loops and branches.

--- src/main.zig
+++ src/main.zig
@@ -51,7 +51,7 @@ pub fn main() !void {
     ;
     print("\n{s}\n", .{SQL});
 
-    // TUPLES, ARRAYS, VECTORS, DESTRUCTURING
+    // TUPLES, ARRAYS, SLICES, VECTORS, DESTRUCTURING
     var x: u32 = undefined;
     var y: u32 = undefined;
     var z: u32 = undefined;
@@ -62,11 +62,96 @@ pub fn main() !void {
     const myArray = [_]u32{ 3, 4, 5 }; //arrays need a defined type
     x, y, z = myArray; //same thing
 
+    // [TO DO]: talk about slices in more detail, slices vs arrays, etc.
+
     const myVector: @Vector(3, u32) = .{ 6, 7, 8 }; //vectors need a defined length using a comptime int, and defined type
     x, y, z = myVector; //same thing
     print("\nx:{},y:{},z:{}\n", .{ x, y, z }); //what we've been using for this have just been tuples!
     print("x:{},y:{}\n", myTuple); //same thing!
 
+    // BRANCHES, LOOPS
+
+    // if
+    var isItDone: bool = false;
+    if (isItDone) {
+        print("It's done.\n", .{});
+    } else if (!isItDone) {
+        print("It's not done.\n", .{});
+        isItDone = true;
+        print("Now it's done.\n", .{});
+    }
+
+    // switch
+    const myInt: u8 = 3;
+    switch (myInt) {
+        0...3 => print("Between 0 and 3\n", .{}),
+        4...9 => print("Between 4 and 9\n", .{}),
+        else => print("Bro too big, I dunno...\n", .{}),
+    }
+
+    // while loop
+    var i: usize = 0;
+    while (i < 10) {
+        i += 1;
+    }
+    try std.testing.expect(i == 10); //testing expect panics if wrong, does nothing if right
+
+    // break
+    while (isItDone) {
+        if (i > 15) {
+            break;
+        }
+        i += 1;
+    }
+    try std.testing.expect(i == 16);
+
+    // for loop
+    const items = [_]i32{ 1, 2, 0, 4, 5, 6 };
+    var sum: i32 = 0;
+    for (items) |value| {
+        if (value == 0) {
+            continue;
+        } else if (value == 5) {
+            break;
+        }
+        sum += value;
+    }
+    print("{}\n", .{sum}); //1+2+4=7
+    // accessing indices
+    var sum2: i32 = 0;
+    for (items, 0..) |_, j| {
+        print("{}\n", .{j});
+        sum2 += @as(i32, @intCast(j));
+    }
+    print("{}\n", .{sum2}); //0+1+2+3+4+5=15
+
+    // multi-object for (zip)
+    const items2 = [_]i32{ 6, 5, 4, 0, 2, 6 };
+    for (items, items2) |value1, value2| { //need to be same length
+        if (value1 == value2) {
+            print("Found a match! Value1: {}, Value2: {}\n", .{ value1, value2 });
+        }
+    }
+
+    // for by reference
+    var items3 = [_]i32{ 2, 3, 1 };
+    for (&items3) |*value| {
+        value.* += 2;
+    }
+
+    try std.testing.expect(items3[0] == 4);
+    try std.testing.expect(items3[1] == 5);
+    try std.testing.expect(items3[2] == 3);
+
+    // labels
+    var count: usize = 0;
+    outer: for (1..6) |_| { //label the loop
+        for (1..6) |_| {
+            count += 1;
+            break :outer; //can break the top loop - amazing!
+        }
+    }
+    try std.testing.expect(count == 1);
 }
 
 test "simple test" { //tests run with "zig test ./src/main.zig"
